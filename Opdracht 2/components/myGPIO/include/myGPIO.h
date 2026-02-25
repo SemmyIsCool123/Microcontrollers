a00e37f93d5b470bd8f92417492602fca523a3fe
Jules Van Belle <Julesvanbelle@gmail.com>
2026-02-25 14:14:18 +0100

    Hey

--- Opdracht 2/components/myGPIO/include/myGPIO.h
+++ Opdracht 2/components/myGPIO/include/myGPIO.h
@@ -1,1 +1,9 @@
 void func(void);
+void mygpio_LED_aan(int pin);
+void mygpio_LED_uit(int pin);
+void mygpio_LED_setup(int pin);
+void mygpio_LED_toggle(int pin);
+
+void mygpio_KNOP_setup(int pin);
+void mygpio_KNOP_lees(int pin);
+void mygpio_LED_flank(int pin, int preValue);
